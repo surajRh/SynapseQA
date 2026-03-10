2fa0f23405a521cb178c4795a8ec52ddc29bc015
Suraj <surajrh45@gmail.com>
2026-03-10 10:17:16 +0530

    added logger instance and a locator

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,16 +4,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utils.ConfigReader;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.time.Duration;
 
 public class BaseTest {
 
     protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    protected static final Logger log = LogManager.getLogger(BaseTest.class);
 
     public void setup() {
 
         String browser = ConfigReader.getProperty("browser");
+        log.info("Launching browser" + browser);
 
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
@@ -27,6 +30,7 @@ public class BaseTest {
                 .implicitlyWait(Duration.ofSeconds(
                         Integer.parseInt(ConfigReader.getProperty("timeout"))));
 
+        log.info("Opening url" + ConfigReader.getProperty("url"));
         getDriver().get(ConfigReader.getProperty("url"));
     }
 
--- src/test/java/pages/Loginpage.java
+++ src/test/java/pages/Loginpage.java
@@ -4,13 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 import static org.bouncycastle.cms.RecipientId.password;
 
+
 public class Loginpage {
     WebDriver driver;
+    private static final Logger log = LogManager.getLogger(Loginpage.class);
 
     public Loginpage(WebDriver driver){
         this.driver=driver;
@@ -21,14 +25,17 @@ public class Loginpage {
     private By loginbutton= By.xpath("//button[@data-qa='login-button']");
 
     public void enterEmail(String email){
+        log.info("Entering email");
         driver.findElement(emailField).sendKeys(email);
     }
 
     public void enterPassword(String password){
+        log.info("Entering password");
         driver.findElement(passwordField).sendKeys(password);
     }
 
     public void clickLoginButton(){
+        log.info("Click login button");
         driver.findElement(loginbutton).click();
     }
 
@@ -39,14 +46,18 @@ public class Loginpage {
     }
 
     public void verifyPageTitleHeading() {
-        System.out.print("page tite is :" + driver.getTitle());
+    //    System.out.print("page tite is :" + driver.getTitle());
+        log.info("Page title is" + driver.getTitle());
         Assert.assertTrue(driver.getTitle().contains("Automation Exercise"));
+        log.info("Page title is read successfully");
     }
 
     public void validateTotalLinksinTestPage() {
-        driver.findElement(By.xpath("(//button[contains(text(),'Test Cases')])[1]")).click();
+       // log.info("Navigating to Next page");
+        driver.findElement(By.xpath("//a[contains(text(),' Test Cases')]")).click();
         List<WebElement> allLinks = driver.findElements(By.xpath("//div[@class='panel-group']"));
-        System.out.println("total links :" + allLinks.size());
+      //  System.out.println("total links :" + allLinks.size());
+        log.info("Total links: " +allLinks.size());
 
         for (WebElement link : allLinks) {
             String linkText = link.getText();
